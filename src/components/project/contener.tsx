35aaef8aa27990eba7760b80861642b8eefd07fc
Muhammad <Muhammad@mx>
2023-12-21 15:23:32 +0500

    fin5

--- src/components/project/contener.tsx
+++ src/components/project/contener.tsx
@@ -1,69 +1,131 @@
-import React, { useState } from 'react';
-import { toast, Toaster } from 'react-hot-toast';
-import deleteImage from '../delete.png';
+import React, { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
+import deleteImage from "../delete.png";
 
 function Navbar() {
-  const [newTodo, setNewTodo] = useState('');
-  const [todos, setTodos] = useState<string[]>([]);
+	const [newTodo, setNewTodo] = useState("");
+	const [todos, setTodos] = useState<string[]>([]);
+	const [editableTodoIndex, setEditableTodoIndex] = useState<number | null>(null);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewTodo(event.target.value);
-  };
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setNewTodo(event.target.value);
+	};
 
-  const handleAddTodo = () => {
-    if (newTodo.trim() !== '') {
-      setTodos([...todos, newTodo]);
-      setNewTodo('');
+	const handleAddTodo = () => {
+		if (newTodo.trim() !== "") {
+			setTodos([...todos, newTodo]);
+			setNewTodo("");
+			toast.success("Todo added successfully!");
+		} else {
+			toast.error("Please enter a todo.");
+		}
+	};
 
-      toast.success('Todo added successfully!');
-    } else {
-      toast.error('Please enter a todo.');
-    }
-  };
+	const handleDeleteTodo = (index: number) => {
+		const updatedTodos = [...todos];
+		updatedTodos.splice(index, 1);
+		setTodos(updatedTodos);
+		setEditableTodoIndex(null); // Reset editableTodoIndex when deleting a todo
+		toast.success("Todo deleted successfully!");
+	};
 
-  const handleDeleteTodo = (index: number) => {
-    const updatedTodos = [...todos];
-    updatedTodos.splice(index, 1);
-    setTodos(updatedTodos);
+	const handleEditTodo = (index: number) => {
+		setEditableTodoIndex(index);
+		setNewTodo(todos[index]);
+	};
 
-    toast.success('Todo deleted successfully!');
-  };
+	const handleSaveEdit = () => {
+		if (newTodo.trim() !== "") {
+			const updatedTodos = [...todos];
+			updatedTodos[editableTodoIndex!] = newTodo;
+			setTodos(updatedTodos);
+			setNewTodo("");
+			setEditableTodoIndex(null);
+			toast.success("Todo edited successfully!");
+		} else {
+			toast.error("Please enter a valid todo.");
+		}
+	};
 
-  const handleSaqlashButtonClick = () => {
-    handleAddTodo();
-  };
+	const handleCancelEdit = () => {
+		setEditableTodoIndex(null);
+		setNewTodo("");
+	};
 
-  return (
-    <div className="w-[100%] gap-3 grid place-items-center">
-      <div className="pt-10 gap-3 grid place-items-center h-[auto]">
-        <h1 className="font-sans italic text-[50px]">Todo List</h1>
-        <span className="flex items-center gap-4 mt-4">
-          <input
-            className="w-[300px] px-8 py-2 bor rounded-xl bg-slate-600 placeholder:italic placeholder:text-white text-white"
-            type="text"
-            placeholder="So'z kiriting"
-            value={newTodo}
-            onChange={handleInputChange}
-          />
-          <button
-            className="px-5 py-2 bg-slate-600 text-yellow-50 rounded-xl italic hover:-translate-y-1 active:scale-90"
-            onClick={handleSaqlashButtonClick}
-          >
-            Saqlash
-          </button>
-        </span>
-        <ul className="mt-4 gap-3">
-          {todos.map((todo, index) => (
-            <li key={index} className="text-white flex gap-5  px-3 py-2 w-[200px] bg-slate-500 justify-between">
-              {todo}
-              <img className='cursor-pointer' src={deleteImage} alt="Delete" onClick={() => handleDeleteTodo(index)} />
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Toaster position="top-right" reverseOrder={false} />
-    </div>
-  );
+	return (
+		<div className="w-[100%] gap-3 grid place-items-center">
+			<div className="pt-10 gap-3 grid place-items-center h-[auto]">
+				<h1 className="font-sans italic text-[50px]">Todo List</h1>
+				<span className="flex items-center gap-4 mt-4">
+					<input
+						className="w-[300px] px-8 py-2 bor rounded-xl bg-slate-600 placeholder:italic placeholder:text-white text-white"
+						type="text"
+						placeholder="So'z kiriting"
+						value={newTodo}
+						onChange={handleInputChange}
+					/>
+					{editableTodoIndex !== null ? (
+						<>
+							<button
+								className="px-3 py-2 bg-slate-600 text-yellow-50 rounded-xl italic hover:-translate-y-1 active:scale-90"
+								onClick={handleSaveEdit}
+							>
+								Saqlash
+							</button>
+							<button
+								className="px-3 py-2 bg-slate-600 text-yellow-50 rounded-xl italic hover:-translate-y-1 active:scale-90"
+								onClick={handleCancelEdit}
+							>
+								Bekor qilish
+							</button>
+						</>
+					) : (
+						<button
+							className="px-5 py-2 bg-slate-600 text-yellow-50 rounded-xl italic hover:-translate-y-1 active:scale-90"
+							onClick={handleAddTodo}
+						>
+							Saqlash
+						</button>
+					)}
+				</span>
+				<ul className="mt-4 gap-3">
+					{todos.map((todo, index) => (
+						<li
+							key={index}
+							className={`text-white flex gap-5 px-3 py-2 w-[400px] bg-slate-500 justify-between ${
+								editableTodoIndex === index ? "bg-slate-700" : ""
+							}`}
+						>
+							{editableTodoIndex === index ? (
+								<p className="w-[150px] px-2 py-1 rounded-md bg-slate-600 text-white">{newTodo}</p>
+							) : (
+								<span>{todo}</span>
+							)}
+
+							<span className="flex gap-1 items-center">
+								{editableTodoIndex !== index && (
+									<img
+										id="pen"
+										className="cursor-pointer w-[25px]"
+										src="https://cdn-icons-png.flaticon.com/512/1860/1860115.png"
+										alt=""
+										onClick={() => handleEditTodo(index)}
+									/>
+								)}
+								<img
+									className="cursor-pointer"
+									src={deleteImage}
+									alt="Delete"
+									onClick={() => handleDeleteTodo(index)}
+								/>
+							</span>
+						</li>
+					))}
+				</ul>
+			</div>
+			<Toaster position="top-right" reverseOrder={false} />
+		</div>
+	);
 }
 
 export default Navbar;
