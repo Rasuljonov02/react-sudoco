87bcf842a1276cb3483a7db689c278c1ef0bb33e
Muhammad <Muhammad@mx>
2023-12-25 15:02:13 +0500

    start1

--- src/components/project/contener.tsx
+++ src/components/project/contener.tsx
@@ -1,32 +1,49 @@
 import React from "react";
 
 function Contac() {
-	const divCount = 9;
+  const divCount = 9;
 
-	const divElements = [];
-	for (let index = 0; index < divCount; index++) {
-		divElements.push(
-			<div key={index} className="childDiv border-solid border-2 border-indigo-300">
-				{index + 1}
-			</div>
-		);
-	}
+  const celDiv = (i: number) => {
+    console.log("id =", i);
+  };
 
-	return (
-		<div className="  w-[100%] grid place-items-center h-[100vh]">
-			<div className=" grid border-solid border-2 border-indigo-500  grid-cols-3 grid-rows-3  w-[600px] h-[600px]">
-				<div className="border-solid border-2 grid place-items-center border-indigo-900  grid  grid-cols-3 grid-rows-3 cursor-pointer">{divElements}</div>
-				<div className="border-solid border-2 grid place-items-center border-indigo-900  grid  grid-cols-3 grid-rows-3 cursor-pointer">{divElements}</div>
-				<div className="border-solid border-2 grid place-items-center border-indigo-900  grid  grid-cols-3 grid-rows-3 cursor-pointer">{divElements}</div>
-				<div className="border-solid border-2 grid place-items-center border-indigo-900  grid  grid-cols-3 grid-rows-3 cursor-pointer">{divElements}</div>
-				<div className="border-solid border-2 grid place-items-center border-indigo-900  grid  grid-cols-3 grid-rows-3 cursor-pointer">{divElements}</div>
-				<div className="border-solid border-2 grid place-items-center border-indigo-900  grid  grid-cols-3 grid-rows-3 cursor-pointer">{divElements}</div>
-				<div className="border-solid border-2 grid place-items-center border-indigo-900  grid  grid-cols-3 grid-rows-3 cursor-pointer">{divElements}</div>
-				<div className="border-solid border-2 grid place-items-center border-indigo-900  grid  grid-cols-3 grid-rows-3 cursor-pointer">{divElements}</div>
-				<div className="border-solid border-2 grid place-items-center border-indigo-900  grid  grid-cols-3 grid-rows-3 cursor-pointer">{divElements}</div>
-			</div>
-		</div>
-	);
+  const generateSudokuBoard = () => {
+    const divElements = [];
+    // div children
+    for (let i = 0; i < divCount; i++) {
+      divElements.push(
+        <div
+          key={i}
+          onClick={() => celDiv(i+1)}
+          className="childDiv cursor-pointer grid place-items-center bg-white w-[65px] h-[65px] gap-1"
+        >
+          {i + 1}
+        </div>
+      );
+    }
+    // katadiv
+    const rowElements = [];
+    for (let i = 0; i < divCount; i++) {
+      rowElements.push(
+        <div
+          key={i}
+          className="grid grid-cols-3 grid-rows-3 border border-black bg-black w-[200px] h-[200px]"
+        >
+          {divElements}
+        </div>
+      );
+    }
+
+    return rowElements;
+  };
+
+  return (
+    <div className="w-[100%] grid place-items-center h-[100vh]">
+      <div className="grid grid-cols-3 grid-rows-3 border border-black w-[600px] h-[600px]">
+        {generateSudokuBoard()}
+      </div>
+    </div>
+  );
 }
 
 export default Contac;
